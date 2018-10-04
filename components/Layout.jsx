a32a81599213445597b315084982846782d48f08
gooaya <37108352+gooaya@users.noreply.github.com>
2018-10-04 23:53:29 +1000

    add github icon

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,6 +10,12 @@ import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import IconButton from '@material-ui/core/IconButton';
+
+const GithubIcon = () => (<SvgIcon>
+  <path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"/>
+</SvgIcon>);
 
 const styles = theme => ({
   appBar: {
@@ -20,6 +26,9 @@ const styles = theme => ({
   },
   heroUnit: {
     backgroundColor: theme.palette.background.paper,
+  },
+  grow: {
+    flex: '1 1 auto',
   },
   heroContent: {
     maxWidth: 600,
@@ -58,6 +67,16 @@ function Layout(props) {
               Mikado Mochi Shop
             </Typography>
           </Link>
+          <div className={classes.grow} />
+          <IconButton
+            component="a"
+            color="inherit"
+            target="blank"
+            href="https://github.com/gooaya/mikado-mochi-shop"
+            aria-label="GitHub repository"
+          >
+            <GithubIcon/>
+          </IconButton>
         </Toolbar>
       </AppBar>
       <main>
